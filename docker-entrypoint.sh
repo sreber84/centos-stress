280e61abdaf2622710bba28b4f53125087b3f076
Sebastian Jug <sejug@redhat.com>
2016-08-18 08:09:24 -0400

    push up logs

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -17,12 +17,13 @@ case "${RUN}" in
     [[ "${ROUTER_IP}" ]] && echo "${ROUTER_IP} ${TARGET_HOST}" >> /etc/hosts
 
     while [[ -z $(curl -s http://"${GUN}":9000) ]]; do sleep 5; echo "not ready"; done
-    exec jmeter -n -t test.jmx -Jnum=${NUM} -Jramp=${JMETER_RAMP} \
+    sh jmeter.sh -n -t test.jmx -Jnum=${NUM} -Jramp=${JMETER_RAMP} \
       -Jduration=${JMETER_TIME} -Jtps=${JMETER_TPS} -Jipaddr1=${TARGET[0]} \
       -Jipaddr2=${TARGET[1]} -Jipaddr3=${TARGET[2]} -Jipaddr4=${TARGET[3]} \
       -Jipaddr5=${TARGET[4]} -Jipaddr6=${TARGET[5]} -Jipaddr7=${TARGET[6]} \
       -Jipaddr8=${TARGET[7]} -Jipaddr9=${TARGET[8]} -Jport=${TARGET_PORT} \
-      -l testplan.jtl -j testplan.log
+      -l jmeter-"${HOSTNAME}"-"$(date +%y%m%d%H%M)".jtl -j tjmeter-"${HOSTNAME}"-"$(date +%y%m%d%H%M)".log
+    scp *.jtl ${GUN}:~/ && scp *.log ${GUN}:~/
     ;;
   *)
     echo "Need to specify what to run."
