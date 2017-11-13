07aafd362638235d82d2b2c4d572080417415ad0
Jiri Mencak <jmencak@redhat.com>
2017-11-13 16:09:33 +0100

    Arbitrary uid (OpenShift deployments) support.

--- root/gnuplot/mb/graph.sh
+++ root/gnuplot/mb/graph.sh
@@ -71,7 +71,6 @@ graph_d3js_xy() {
 </html>
 EOF
 }
-
 graph_total_latency_pctl() {
   local graph_dir="$1"
   local results="$2"
@@ -79,17 +78,17 @@ graph_total_latency_pctl() {
   local graph_conf=$(realpath ${graph_dir}/total_latency_pctl.conf)
   local graph_data_in=$dir_out/total_latency_pctl.txt
   local graph_image_out=$dir_out/total_latency_pctl.png
-
+ 
   mkdir -p $dir_out
   rm -f $graph_data_in
   printf "# http_status	90%%	95%%	99%%\n" > $graph_data_in
-  for err in $(awk -F, '{print $2}' $results | sort -u)
+  for err in $(awk -F, '{print $3}' $results | sort -u)
   do
     printf "%s\t" $err >> $graph_data_in
-    awk -F, "{if(\$2 == $err) {print (\$3/1000000)}}" < $results  | \
+    awk -F, "{if(\$3 == $err) {print (\$2/1000)}}" < $results  | \
       $pctl_bin -l $pctls >> $graph_data_in
   done
-
+  
   gnuplot \
     -e "data_in='$graph_data_in'" \
     -e "graph_out='$graph_image_out'" \
@@ -102,8 +101,11 @@ graph_time_bytes_hits_latency() {
   local dir_out="$3"
   local graph_data_in=$dir_out/time_bhl.txt
 
-  local graph_bytes_conf=$(realpath ${graph_dir}/time_bytes.conf)
-  local graph_image_out_bytes=$dir_out/time_bytes.png
+  local graph_bytes_conf_out=$(realpath ${graph_dir}/time_bytes_out.conf)
+  local graph_image_out_bytes_out=$dir_out/time_bytes_out.png
+
+  local graph_bytes_conf_in=$(realpath ${graph_dir}/time_bytes_in.conf)
+  local graph_image_out_bytes_in=$dir_out/time_bytes_in.png
 
   local graph_hits_conf=$(realpath ${graph_dir}/time_hits.conf)
   local graph_image_out_hits=$dir_out/time_hits.png
@@ -112,11 +114,11 @@ graph_time_bytes_hits_latency() {
   local graph_image_out_latency=$dir_out/time_latency.png
 
   mkdir -p $dir_out
-  printf "# timestamp_s	bytes_out	bytes_in	hits	latency\n" > $graph_data_in
+  printf "# time	bytes_out	bytes_in	hits	latency\n" > $graph_data_in
   awk '
 {
-  time=int($1/1000000000)	# get seconds [original value in ns]
-  latency += ($3/1000000)	# get miliseconds [original value in ns]
+  time=int($1/1000000)	# get seconds [original value in us]
+  latency += ($2/1000)	# get miliseconds [original value in us]
   bytes_out += $4
   bytes_in += $5
   hits += 1
@@ -131,7 +133,7 @@ graph_time_bytes_hits_latency() {
     hits=0
     latency=0
   }
-}
+} 
 BEGIN {
   FS=","
   time_prev=0
@@ -144,8 +146,13 @@ BEGIN {
 
   gnuplot \
     -e "data_in='$graph_data_in'" \
-    -e "graph_out='$graph_image_out_bytes'" \
-    $graph_bytes_conf
+    -e "graph_out='$graph_image_out_bytes_out'" \
+    $graph_bytes_conf_out
+
+  gnuplot \
+    -e "data_in='$graph_data_in'" \
+    -e "graph_out='$graph_image_out_bytes_in'" \
+    $graph_bytes_conf_in
 
   gnuplot \
     -e "data_in='$graph_data_in'" \
@@ -170,12 +177,12 @@ graph_total_hits() {
   rm -f $graph_data_in
 
   printf "# http_status	count\n" > $graph_data_in
-  for err in $(awk -F, '{print $2}' $results | sort -n -u)
+  for err in $(awk -F, '{print $3}' $results | sort -n -u)
   do
     printf "%s\t" $err >> $graph_data_in
     awk -F, "
 {
-  if(\$2 == $err) {i++}
+  if(\$3 == $err) {i++}
 }"'
 BEGIN {
   i=0
@@ -191,12 +198,12 @@ END {
     $graph_conf
 }
 
-graph_time_bytes_per_endpoint() {
+graph_time_bytes_in_per_endpoint() {
   local graph_dir="$1"
   local results="$2"
   local dir_out="$3"
   local interval="$4"	# sample interval for d3js graphs [s]
-  local graph_data_base=time_bytes_per_endpoint
+  local graph_data_base=time_bytes_in_per_endpoint
   local graph_data_in=$dir_out/${graph_data_base}.csv
   local unique_endpoints=unique_endpoints.txt
 
@@ -204,7 +211,7 @@ graph_time_bytes_per_endpoint() {
   rm -f $graph_data_in
   graph_d3js_time "Bytes received by client per second" ${graph_data_base}.html ${graph_data_in##*/} $dir_out
 
-  awk -F, '{a[$7]}END{for (v in a) {print v}}' $results | LC_ALL=C sort > $unique_endpoints
+  awk -F, '{a[$6]}END{for (v in a) {print v}}' $results | LC_ALL=C sort > $unique_endpoints
   awk -F, -v "intvl=${interval:-1}" '
 function print_header(name) {
   printf "timestamp_ms" 
@@ -234,7 +241,7 @@ function clear_stats() {
 }
 
 function main(name) {
-  time=int($1/1000000000/intvl)*intvl	# get seconds [original value in ns]
+  time=int($1/1000000/intvl)*intvl	# get seconds [original value in us]
   a[name]["bytes"]+=$5			# get bytes in
   a[name]["hits"]++
 
@@ -255,7 +262,7 @@ NR==FNR{ # fill a[] with keys from the first file
 }
 FNR==1{print_header()}			# just started processing the second file ($results)
 
-main($7)				# processing of the second file
+main($6)				# processing of the second file
 
 BEGIN {
   time_prev=0
@@ -281,7 +288,7 @@ graph_time_hits_per_endpoint() {
   rm -f $graph_data_in
   graph_d3js_time "Client requests per second" ${graph_data_base}.html ${graph_data_in##*/} $dir_out
 
-  awk -F, '{a[$7]}END{for (v in a) {print v}}' $results | LC_ALL=C sort > $unique_endpoints
+  awk -F, '{a[$6]}END{for (v in a) {print v}}' $results | LC_ALL=C sort > $unique_endpoints
   awk -F, -v "intvl=${interval:-1}" '
 function print_header(name) {
   printf "timestamp_ms" 
@@ -310,7 +317,7 @@ function clear_stats() {
 }
 
 function main(name) {
-  time=int($1/1000000000/intvl)*intvl	# get seconds [original value in ns]
+  time=int($1/1000000/intvl)*intvl	# get seconds [original value in us]
   a[name]["hits"]++
 
   if(time_prev == 0) {
@@ -329,7 +336,7 @@ NR==FNR{ # fill a[] with keys from the first file
 }
 FNR==1{print_header()}			# just started processing the second file ($results)
 
-main($7)				# processing of the second file
+main($6)				# processing of the second file
 
 BEGIN {
   time_prev=0
@@ -355,7 +362,7 @@ graph_time_latency_per_endpoint() {
   rm -f $graph_data_in
   graph_d3js_time "Latency [ms]" ${graph_data_base}.html ${graph_data_in##*/} $dir_out
 
-  awk -F, '{a[$7]}END{for (v in a) {print v}}' $results | LC_ALL=C sort > $unique_endpoints
+  awk -F, '{a[$6]}END{for (v in a) {print v}}' $results | LC_ALL=C sort > $unique_endpoints
   awk -F, -v "intvl=${interval:-1}" '
 function print_header(name) {
   printf "timestamp_ms" 
@@ -385,8 +392,8 @@ function clear_stats() {
 }
 
 function main(name) {
-  time=int($1/1000000000/intvl)*intvl	# get seconds [original value in ns]
-  a[name]["latency"]+=$3/1000000	# get miliseconds [original value in ns]
+  time=int($1/1000000/intvl)*intvl	# get seconds [original value in us]
+  a[name]["latency"]+=$2/1000		# get miliseconds [original value in us]
   a[name]["hits"]++
 
   if(time_prev == 0) {
@@ -406,7 +413,7 @@ NR==FNR{ # fill a[] with keys from the first file
 }
 FNR==1{print_header()}			# just started processing the second file ($results)
 
-main($7)				# processing of the second file
+main($6)				# processing of the second file
 
 BEGIN {
   time_prev=0
@@ -419,33 +426,6 @@ END {
   rm -f $unique_endpoints 
 }
 
-# TODO: atrociasly slow, write in Golang
-#graph_total_latency_per_endpoint() {
-#  local graph_dir="$1"
-#  local results="$2"
-#  local dir_out="$3"
-#  local graph_data_base=total_latency_per_endpoint
-#  local graph_data_in=$dir_out/${graph_data_base}.csv
-#  local unique_endpoints=unique_endpoints.txt
-#  local title_x=Percentiles
-#  local paste_a=$(mktemp paste-a.XXXXXX)
-#  local paste_b=$(mktemp paste-b.XXXXXX)
-#
-#  mkdir -p $dir_out
-#  rm -f $graph_data_in
-#  graph_d3js_xy "Server latency percentiles [ms]" ${graph_data_base}.html ${graph_data_in##*/} $dir_out
-#
-#  awk -F, '{a[$7]}BEGIN{printf "'"$title_x"'"}END{for (v in a) {printf ",%s",v}; print ""}' $results | LC_ALL=C sort > $unique_endpoints
-#  seq 1 100 > $paste_a
-#  while read -d, v ; do
-#    test ",$title_x," = ",$v," && continue
-#    awk -F, '{if ($7 == "'$v'") {print $3}}' $results | pctl -l -d '\n' > $paste_b
-#    paste -d, $paste_a $paste_b > $graph_data_in
-#    cp $graph_data_in $paste_a
-#  done < $unique_endpoints
-#  rm -f $unique_endpoints $paste_a $paste_b 
-#}
-
 main() {
   local graph_dir="${1:-.}"
   local results_csv="${2:-results_sample.csv}"
@@ -457,11 +437,9 @@ main() {
   graph_time_bytes_hits_latency $graph_dir $results_csv $dir_out
   graph_total_hits $graph_dir $results_csv $dir_out
 
-  graph_time_bytes_per_endpoint $graph_dir $results_csv $dir_out $interval
+  graph_time_bytes_in_per_endpoint $graph_dir $results_csv $dir_out $interval
   graph_time_hits_per_endpoint $graph_dir $results_csv $dir_out $interval
   graph_time_latency_per_endpoint $graph_dir $results_csv $dir_out $interval
-
-#  graph_total_latency_per_endpoint $graph_dir $results_csv $dir_out 
 }
 
 main "$@"
